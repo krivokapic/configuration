6743a6a27c1a169b033e7ab990845e378c338a89
Vladimir Krivokapic <krivokapicvlada10@gmail.com>
2026-08-01 22:49:15 +0200

    rm files

--- nvim/lua/plugins/bfq.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "kevinhwang91/nvim-bqf",
-        dependencies = {},
-        opts = {},
-        config = function()
-        end,
-    }
-}
--- nvim/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "Mofiqul/vscode.nvim", 
-    name = "vscode",
-    priority = 1000, 
-    config = function()
-        require("vscode").setup({})
-        vim.cmd.colorscheme("vscode")
-    end,
-}
